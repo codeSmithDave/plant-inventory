17be53494047fa81aee1851b190fe59950d23ea8
Dave Puinean <davidpuinean@gmail.com>
2025-06-10 15:17:29 -0600

    added react-data-table-component and sample plant data

---
+++ src/types/Plant.ts
@@ -0,0 +1,10 @@
+interface Plant{
+    id: number,
+    scientificName: string,
+    verbatimTaxonRanks: string,
+    taxonomicStatus: string,
+    taxonRemarks: string,
+    references: string,
+}
+
+// verbatimtaxonranks, taxonomisstatus, taxonremarks, references
